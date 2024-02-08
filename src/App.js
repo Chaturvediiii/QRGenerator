9fa8b380f3cb2663c30fbe317fe3cfd4ac76a4e4
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-02-08 12:37:04 +0530

    bg

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
     error
   };
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 px-2">
+    <div className="bg-gray-800 h-screen pt-36 px-2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
         <div className="md:grid md:grid-cols-3">
           <InputContext.Provider value={value}>
